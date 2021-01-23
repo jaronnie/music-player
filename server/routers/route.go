c6114af61393b466262aa1caad16e483c5fcf095
cloudcoder <nj788229@gmail.com>
2021-01-23 23:00:04 +0800

    System of routers

--- server/routers/route.go
+++ server/routers/route.go
@@ -1,22 +1,15 @@
 package routers
 
 import (
-
 	"github.com/gin-gonic/gin"
-
 	"github.com/nj-jay/music-player/server/controllers"
-
-
+	"github.com/nj-jay/music-player/server/ws"
 )
 
 func LoadRouter(e *gin.Engine) {
-
 	e.GET("/api/v1/list", controllers.QueryAllData)
-
 	e.GET("/api/v1/search", controllers.QueryDataByName)
-
 	e.GET("/api/v1/song/url", controllers.PlayMusic)
-
 	e.GET("/api/v1/click", controllers.ClickCount)
-
+	e.GET("/ws", ws.HandlerChat)
 }
