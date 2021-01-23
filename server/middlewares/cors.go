cb59cb6d7f3108dc9e1b3543f1bb60d7707f11a3
cloudcoder <nj788229@gmail.com>
2021-01-23 22:59:48 +0800

    System of middlewares

--- server/middlewares/cors.go
+++ server/middlewares/cors.go
@@ -1,20 +1,14 @@
 package middlewares
 
 import (
-
 	"github.com/gin-gonic/gin"
-
 	"net/http"
-
 )
 
 //解决跨域问题
 func Cors() gin.HandlerFunc {
-
 	return func(c *gin.Context) {
-
 		method := c.Request.Method
-
 		// 必须，指定允许的域名
 		c.Header("Access-Control-Allow-Origin", "*")
 		// 可选，指定允许的请求方式
@@ -25,14 +19,9 @@ func Cors() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Credentials", "true")
 		// 可选，指定时间内减少发送「预检」请求
 		c.Header("Access-Control-Max-Age", "60")
-
 		if method == "OPTIONS" {
-
 			c.AbortWithStatus(http.StatusNoContent)
-
 		}
-
 		c.Next()
-
 	}
 }
