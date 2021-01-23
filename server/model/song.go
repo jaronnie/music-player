f8cb7bd0a0be43c60b23f803105fc1cf45ee0f9d
cloudcoder <nj788229@gmail.com>
2021-01-23 22:58:06 +0800

    System of models

--- server/model/song.go
+++ server/model/song.go
@@ -1,23 +1,14 @@
 package model
 
 type Song struct {
-
 	Id uint `json:"id" gorm:"primaryKey"`
-
 	Name string `json:"name"`
-
 	Url string `json:"url"`
-
 }
 
 func NewSong(name, url string) *Song {
-
 	return &Song{
-
 		Name: name,
-
 		Url: url,
-
 	}
-
 }
